88afa04d4be4e77535ab039e2aaae61662a2e7a1
generalic <nokmote@gmail.com>
2016-01-14 19:45:10 +0100

    setting animation added

--- src/start/settings_fxml/SettingsScreenController.java
+++ src/start/settings_fxml/SettingsScreenController.java
@@ -1,6 +1,8 @@
 package start.settings_fxml;
 
 import hr.fer.zemris.game.environment.Constants;
+import javafx.animation.Interpolator;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -8,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class SettingsScreenController {
 
@@ -75,9 +78,24 @@ public class SettingsScreenController {
 		group.getChildren().forEach(c -> c.setVisible(false));
 		group.getChildren().add(root);
 
+		ScaleTransition transition = new ScaleTransition(Duration.seconds(1), root);
+		transition.setFromX(2);
+		transition.setFromY(2);
+		transition.setToX(1);
+		transition.setToY(1);
+		transition.setInterpolator(Interpolator.EASE_OUT);
+		transition.play();
+
 		backButton.setOnAction(e -> {
 			group.getChildren().remove(root);
 			group.getChildren().forEach(c -> c.setVisible(true));
+
+			ScaleTransition backTransition = new ScaleTransition(Duration.millis(200), group);
+			backTransition.setFromX(15);
+			backTransition.setFromY(15);
+			backTransition.setToX(1);
+			backTransition.setToY(1);
+			backTransition.play();
 		});
 	}
 
